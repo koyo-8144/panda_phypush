287643ce15779e0fc6f222077589055a65b93027
koyo-8144 <koyo.fujii@nottingham.ac.uk>
2026-02-27 23:20:21 +0000

    update

--- collect_model_input_auto.bash
+++ collect_model_input_auto.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # --- 0. EXPERIMENT SETTINGS ---
-OBJECT="nolid_cube_0.598" # "wooden_cube_0.348" # "blue_cylinder_0.175" #"colored_cubes_0.989"
-SURFACE="wood_rough" # "wood_smooth" # "green_rub"
+OBJECT="nolid_cube_1.240" # "wooden_cube_0.348" # "blue_cylinder_0.175" #"colored_cubes_0.989"
+SURFACE="wood_smooth" # "wood_rough" # "green_rub"
 NUM_RUNS=10
 SCRIPT_NAME="collect_model_input.py"
 
